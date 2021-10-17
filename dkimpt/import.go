59b4477fb5c3c24e3d859aa395f1d0c0f0e7ec6d
Cyrille Nofficial <cynoffic@cyrilix.fr>
2021-10-17 19:16:37 +0200

    feat(dkimport): add command to import data from donkey parts

---
+++ dkimpt/import.go
@@ -0,0 +1,166 @@
+package dkimpt
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/cyrilix/robocar-tools/record"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"path"
+	"regexp"
+)
+
+/* donkey import*/
+
+var (
+	camSubDir         = "cam"
+	camIndexRegexp    *regexp.Regexp
+	recordIndexRegexp *regexp.Regexp
+)
+
+func ImportDonkeyRecords(basedir string, destDir string) error {
+	dirItems, err := ioutil.ReadDir(basedir)
+	if err != nil {
+		return fmt.Errorf("unable to list directory in %v dir: %v", basedir, err)
+	}
+
+	imgCams := make([]string, 0)
+	records := make([]string, 0)
+
+	for _, dirItem := range dirItems {
+		log.Debugf("process %v directory", dirItem)
+		camDir := path.Join(destDir, dirItem.Name(), camSubDir)
+
+		err := os.MkdirAll(camDir, os.FileMode(0755))
+		if err != nil {
+			return fmt.Errorf("unable to make dest directories %v: %v", camDir, err)
+		}
+
+		imgDir := path.Join(basedir, dirItem.Name(), camSubDir)
+		imgs, err := ioutil.ReadDir(imgDir)
+		if err != nil {
+			return fmt.Errorf("unable to list cam images in directory %v: %v", imgDir, err)
+		}
+
+		for _, img := range imgs {
+			idx, err := indexFromFile(camIndexRegexp, img.Name())
+			if err != nil {
+				return fmt.Errorf("unable to find index in cam image name %v: %v", img.Name(), err)
+			}
+			log.Debugf("found image with index %v", idx)
+			records = append(records, path.Join(basedir, dirItem.Name(), fmt.Sprintf(record.RecorNameFormat, idx)))
+			imgCams = append(imgCams, path.Join(basedir, dirItem.Name(), camSubDir, img.Name()))
+		}
+
+		err = copyToDestdir(destDir, dirItem.Name(), &imgCams, &records)
+		if err != nil {
+			log.Warnf("unable to copy files from %v to %v: %v", path.Join(basedir, dirItem.Name()), destDir, err)
+			continue
+		}
+	}
+
+	return nil
+}
+
+func init() {
+	re, err := regexp.Compile("image_array_(?P<idx>[0-9]+)\\.jpg$")
+	if err != nil {
+		log.Fatalf("unable to compile regex: %v", err)
+	}
+	camIndexRegexp = re
+
+	re, err = regexp.Compile("record_(?P<idx>[0-9]+)\\.json$")
+	if err != nil {
+		log.Fatalf("unable to compile regex: %v", err)
+	}
+	recordIndexRegexp = re
+}
+
+func indexFromFile(regex *regexp.Regexp, fileName string) (string, error) {
+	matches := findNamedMatches(regex, fileName)
+	if matches["idx"] == "" {
+		return "", fmt.Errorf("no index in filename")
+	}
+	return matches["idx"], nil
+}
+
+func findNamedMatches(regex *regexp.Regexp, str string) map[string]string {
+	match := regex.FindStringSubmatch(str)
+
+	results := map[string]string{}
+	for i, name := range match {
+		results[regex.SubexpNames()[i]] = name
+	}
+	return results
+}
+
+func copyToDestdir(destdir, dirItem string, imgFiles *[]string, recordFiles *[]string) error {
+	err := copyJsonFiles(destdir, dirItem, recordFiles)
+	if err != nil {
+		return fmt.Errorf("unable to copy json files in %v directory: %v", destdir, err)
+	}
+
+	err = copyCamImages(destdir, dirItem, imgFiles)
+	if err != nil {
+		return fmt.Errorf("unable to copy cam files in %v director: %v", destdir, err)
+	}
+
+	return nil
+}
+
+func copyCamImages(destdir, dirItem string, imgFiles *[]string) error {
+	for _, img := range *imgFiles {
+		imgContent, err := ioutil.ReadFile(img)
+		if err != nil {
+			return fmt.Errorf("unable to read img: %v", err)
+		}
+
+		idx, err := indexFromFile(camIndexRegexp, img)
+		if err != nil {
+			log.Warningf("unable to extract idx from filename %v: %v", img, err)
+			continue
+		}
+		imgName := path.Join(destdir, dirItem, camSubDir, fmt.Sprintf("image_array_%s_%s.jpg", dirItem, idx))
+
+		err = ioutil.WriteFile(imgName, imgContent, os.FileMode(0755))
+		if err != nil {
+			return fmt.Errorf("unable to write image %v: %v", imgName, err)
+		}
+	}
+	return nil
+}
+
+func copyJsonFiles(destdir, dirItem string, recordFiles *[]string) error {
+	for _, r := range *recordFiles {
+		content, err := ioutil.ReadFile(r)
+		if err != nil {
+			return fmt.Errorf("unable to read json content: %v", err)
+		}
+		idx, err := indexFromFile(recordIndexRegexp, r)
+		if err != nil {
+			log.Warningf("unable to extract idx from filename %v: %v", r, err)
+			continue
+		}
+
+		var rcd record.Record
+		err = json.Unmarshal(content, &rcd)
+		if err != nil {
+			return fmt.Errorf("unable to unmarshal record: %v", err)
+		}
+		camName := fmt.Sprintf("image_array_%s_%s.jpg", dirItem, idx)
+		rcd.CamImageArray = path.Join(camSubDir, camName)
+
+		recordBytes, err := json.Marshal(&rcd)
+		if err != nil {
+			return fmt.Errorf("unable to marshal %v record: %v", rcd, err)
+		}
+
+		recordFileName := path.Join(destdir, dirItem, fmt.Sprintf("record_%s_%s.json", dirItem, idx))
+		err = ioutil.WriteFile(recordFileName, recordBytes, os.FileMode(0755))
+		if err != nil {
+			return fmt.Errorf("unable to write json record %v: %v", recordFileName, err)
+		}
+	}
+	return nil
+}
---
+++ dkimpt/import_test.go
@@ -0,0 +1,87 @@
+package dkimpt
+
+import (
+	"encoding/json"
+	record2 "github.com/cyrilix/robocar-tools/record"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"os"
+	"path"
+	"testing"
+)
+
+func TestImportDonkeyRecords(t *testing.T) {
+	destDir, err := ioutil.TempDir("", "test-import")
+	if err != nil {
+		t.Errorf("unable to generate destdir for test")
+	}
+	defer func() {
+		err := os.RemoveAll(destDir)
+		log.Errorf("unable to delete tmpdir %v: %v", destDir, err)
+	}()
+
+	err = ImportDonkeyRecords("testdata", destDir)
+	if err != nil {
+		t.Errorf("unable to import files: %v", err)
+	}
+
+	cases := []struct {
+		setDir          string
+		expectedDir     int
+		expectedRecords int
+	}{
+		{"20191012_111416", 1, 7},
+		{"20191012_122633", 1, 8},
+	}
+
+	for _, c := range cases {
+
+		files, err := ioutil.ReadDir(path.Join(destDir, c.setDir))
+		if err != nil {
+			t.Errorf("[%v] unable to list %v content: %v", c.setDir, destDir, err)
+		}
+
+		dirNames := make([]string, 0)
+		fileNames := make([]string, 0)
+		for _, f := range files {
+			if f.IsDir() {
+				dirNames = append(dirNames, f.Name())
+			} else {
+				fileNames = append(fileNames, f.Name())
+			}
+		}
+
+		if len(dirNames) != c.expectedDir {
+			t.Errorf("[%v] %v dirs found, wants %v", c.setDir, len(dirNames), 1)
+		}
+		if len(fileNames) != c.expectedRecords {
+			t.Errorf("[%v] %v files found, wants %v", c.setDir, len(fileNames), 15)
+		}
+
+		for _, jsonFile := range fileNames {
+			content, err := ioutil.ReadFile(path.Join(destDir, c.setDir, jsonFile))
+			if err != nil {
+				t.Errorf("[%v] unable to read file content of %v: %v", c.setDir, jsonFile, err)
+				continue
+			}
+			var record record2.Record
+			err = json.Unmarshal(content, &record)
+			if err != nil {
+				t.Errorf("[%v] unable to unmarshal file %v with content %v: %v", c.setDir, jsonFile, content, err)
+				continue
+			}
+			if record.UserAngle == 0. {
+				t.Errorf("[%v] user_angle field not initialized for %v file: %v", c.setDir, jsonFile, content)
+			}
+			camPath := path.Join(destDir, c.setDir, record.CamImageArray)
+			camFile, err := os.Stat(camPath)
+			if os.IsNotExist(err) {
+				t.Errorf("[%v] cam image %v doesn't exist for record %v: %v", c.setDir, camPath, jsonFile, err)
+				continue
+			}
+			if camFile.Size() == 0 {
+				t.Errorf("[%v] cam image %v is empty", c.setDir, camFile.Name())
+			}
+		}
+	}
+}
